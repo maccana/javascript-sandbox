adda4c82f99257fcf1b1538c760609eb2a4fc959
maccana <mikeymccann@gmail.com>
2015-04-16 22:31:04 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -8,6 +8,11 @@ function storage() {
 	console.log("Local storage successful!");
 	console.log(key + ': ' + value);
 	document.getElementById("output").innerHTML = 
-	sessionStorage.getItem(key);
-	var value = document.getElementById("text-input").innerHTML = "";
+	sessionStorage.getItem(key) + " Stored in Session";
+	document.getElementById("text-input").value = "";
+}
+function deleteFromSessionStorage() {
+	console.log("Name: " + sessionStorage.getItem("Name"));	
+	sessionStorage.removeItem("Name");
+	document.getElementById("output").innerHTML = "";
 }
