bc46c0872075b8b217fd6762f6d52c534c040941
maccana <mikeymccann@gmail.com>
2015-04-29 11:08:05 -0400

    Updated closures.js

--- js/closures.js
+++ js/closures.js
@@ -1,28 +1,56 @@
 // closures.js
 // Closure examples
 
-console.log("We're ready to create a new cat :D");
+console.log("We're ready to create a new cat :x");
 
-// Closure 
-function buildCat(catName, catColor){
-console.log(catName + " is the name and " + catColor + " is the color");
-	var catName = catName;
-	var catColor = catColor;
+// buildCat closure initializes private variables onload 
+var buildCat = (function(catName, catColor){
+	
+	// local vars - protected from global scope clashes or access
+	var catName = "lucy";
+	var catColor = "white";
+	var catCount = 0;
+	
+	// log default cat stats
+	console.log(catName + " is the name and " + catColor + " is the color");
+	console.log("cnt: " + catCount);
 
-	function catFact() {
+	// catFact only accessible via buildCat
+	return function catFact(catName, catColor) {
 		console.log(catName + " is the new cat's name");
 		console.log(catColor + " is the new cat's color");
-		alert("Hello " +catName);
-
-
+		// simple alert confirms new cat has been created
+		alert("Hello " + catName);
+		// closure structure means catCount is incremented each time catFact is run
+		catCount++; 
+		// log out cat no. to confirm increment is working!
+		console.log("CatNo: " + catCount);
 	}
 	return catFact;
-}
+})();
+
 // Button to access the function and member variables inside the closure
 document.getElementById('cat').onclick = function() {
+	
+	var catName = document.getElementById("cat_name").value;
+	var catColor = document.getElementById("cat_color").value;
 
-	var newCat = buildCat("Orson","Grey");
-	newCat();
+	buildCat(catName, catColor);
 	alert('Cat created successfully!');
-	// Fire him off to the DB here!
-}
+	// return cat JSON object here and fire him off to the DB!
+
+// closure structure for change name from default assignment
+var nameChange = (function (name) {
+    var name = "Mike";
+		console.log("Default name: " + name);
+    return function (name) { return name; }
+})();
+
+console.log("Name changed: " + nameChange("Charlie"));
+console.log("Name changed: " + nameChange("Jenny"));
+console.log("Name changed: " + nameChange("Steve"));
+
+
+// add a counter here 
+// http://www.w3schools.com/js/js_function_closures.asp
+
