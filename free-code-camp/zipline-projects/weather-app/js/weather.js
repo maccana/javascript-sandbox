7e64e62826789a938dcdade421e520cf9e000cad
maccana <mikeymccann@gmail.com>
2015-09-29 00:56:14 -0400

    Switch unit selector back to celsius if farenheit when user chooses to load local weather

--- free-code-camp/zipline-projects/weather-app/js/weather.js
+++ free-code-camp/zipline-projects/weather-app/js/weather.js
@@ -12,6 +12,11 @@ $('.js-geolocation').click(function() {
   if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function(position) {
       loadWeather(position.coords.latitude + ',' + position.coords.longitude);
+      // Switch unit selector back to celsius default if currently farenheit
+      if($('.farenheit').hasClass('selected')){ 
+        $('.farenheit').removeClass('selected');
+        $('.celsius').addClass('selected');
+      } 
     });
   } else {
     $(".error").append('<p>Sorry your browser is not compatible.</p>');
@@ -50,14 +55,16 @@ function loadWeather(location, woeid) {
 $('.unit').click(function() {
   var tempToInt = parseInt(tempVal);
   if ($('.celsius').hasClass("selected")) {
-    $('.farenheit').addClass("selected");
     $('.celsius').removeClass("selected");
+    $('.farenheit').addClass("selected");
+    // Convert Celsius to Farenheit and add new value to temperature element
     var celsiusToFarenheit = (tempToInt * 9 / 5) + 32 + '&deg';
     $(".temperature").html(celsiusToFarenheit);
   } else {
     $('.farenheit').removeClass("selected");
     $('.celsius').addClass("selected");
-    var unit = tempToInt + '&deg';
-    $(".temperature").html(unit);
+    // Revert temperature back to default celsius value
+    var celsius = tempToInt + '&deg';
+    $(".temperature").html(celsius);
   }
 });
