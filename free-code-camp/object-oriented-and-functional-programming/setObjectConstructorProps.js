b7b161894ff17e2e069746c4fd6a460db0212e54
maccana <mikeymccann@gmail.com>
2017-05-21 14:57:58 -0400

    Added setObjectConstructorProps.js to FCC solutions

---
+++ free-code-camp/object-oriented-and-functional-programming/setObjectConstructorProps.js
@@ -0,0 +1,16 @@
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+
+/* Have your MotorBike constructor describe an object with wheels, 
+  and seats properties and set them to numbers */
+
+var MotorBike = function() {
+  this.engines = 1;
+  this.wheels = 2;
+  this.seats = 1;
+    
+
+};
