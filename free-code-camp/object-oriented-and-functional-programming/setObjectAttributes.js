8c34c85a731b742d757a8fa46d4776c2bcadf782
maccana <mikeymccann@gmail.com>
2017-05-21 14:47:17 -0400

    Added setObjectAttributes.js to FCC solutions

---
+++ free-code-camp/object-oriented-and-functional-programming/setObjectAttributes.js
@@ -0,0 +1,15 @@
+var car = {
+  "wheels":4,
+  "engines":1,
+  "seats":5
+};
+
+var motorBike = {
+
+  /* Give your motorBike object a wheels, engines and seats attribute 
+     and set them to numbers. */
+  "wheels": 2,
+  "engines": 1,
+  "seats": 1
+
+};
