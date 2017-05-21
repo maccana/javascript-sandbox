679e58abe5c900f81333c223fba462b93e235a4e
maccana <mikeymccann@gmail.com>
2017-05-21 15:03:24 -0400

    Added makeObjectInstanceWithConstructor.js to FCC solutions

---
+++ free-code-camp/object-oriented-and-functional-programming/makeObjectInstanceWithConstructor.js
@@ -0,0 +1,11 @@
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+
+// Make a new Car instance and add a property called nickname with assignment of 
+// the string "myCar" as it's value.
+
+var myCar = new Car();
+myCar.nickname = "myCar";
