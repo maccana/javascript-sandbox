48b5707dcafb25500483b942e24dfa5c216d3b62
maccana <mikeymccann@gmail.com>
2017-05-24 18:37:19 -0400

    Added pass-params-to-constructor.js to FCC

---
+++ free-code-camp/object-oriented-and-functional-programming/pass-params-to-constructor.js
@@ -0,0 +1,8 @@
+var Car = function(wheels, seats, engines) {
+  this.wheels = wheels;
+  this.seats = seats;
+  this.engines = engines;
+};
+
+// Create new Car instance and pass params for wheels, seats and engines
+var myCar = new Car(4, 6, 1);
