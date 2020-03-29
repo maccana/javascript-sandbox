9d1a3952eaad3ddf32243acc620764d56d64d18f
Mike <mikeymccann@gmail.com>
2020-03-29 18:57:02 -0400

    Local Storage test

--- local-storage/local-storage.js
+++ local-storage/local-storage.js
@@ -2,6 +2,13 @@
   if (typeof Storage !== "undefined") {
     // Code for localStorage/sessionStorage.
     console.log("Local Storage supported...");
+    // Sort
+    localStorage.setItem("lastname", "Smith");
+
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem(
+      "lastname"
+    );
   } else {
     alert(
       "Sorry.Your web browser does not support Local Storage.Please choose another brower to continue."
