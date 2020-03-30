6453230f25058a82f2c0c875a80334f5d0401cad
Mike <mikeymccann@gmail.com>
2020-03-30 01:27:29 -0400

    Fixes for storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,27 +1,31 @@
 /* Functions for storage1.html examples of local storage API */
 
 function storage() {
-	console.log("Storage function called...");
-	var key = "Name";
-	var value = document.getElementById("text-input").value;
-	sessionStorage.setItem(key, value);
-	console.log("Local storage successful!");
-	console.log(key + ': ' + value);
-	document.getElementById("output").innerHTML = 
-<<<<<<< HEAD
-	sessionStorage.getItem(key) + "<span class='success'>" + " has been successfully stored in this browser Session" +"</span>";
-	var box = document.getElementById("box");
-	box.style.display = "block";
+  console.log("Storage function called...");
+  var key = "Name";
+  var value = document.getElementById("text-input").value;
+  sessionStorage.setItem(key, value);
+  console.log("Local storage successful!");
+  console.log(key + ": " + value);
+  document.getElementById("output").innerHTML =
+    sessionStorage.getItem(key) +
+    "<span class='success'>" +
+    " has been successfully stored in this browser Session" +
+    "</span>";
+  var box = document.getElementById("box");
+  box.style.display = "block";
 
-=======
-	sessionStorage.getItem(key) + " has been stored in Session";
->>>>>>> 9227bdf349b6d1b48feef0abc2cabcb7c4eef6af
-	document.getElementById("text-input").value = "";
+  sessionStorage.getItem(key) + " has been stored in Session";
+  document.getElementById("text-input").value = "";
 }
 function deleteFromSessionStorage() {
-	console.log("Name: " + sessionStorage.getItem("Name"));
-	var value = sessionStorage.getItem("Name");	
-	sessionStorage.removeItem("Name");
-	document.getElementById("output").innerHTML = "";
-	document.getElementById("output").innerHTML = value + "<span class='success'>" + " has been successfully removed from Session" +"</span>";
+  console.log("Name: " + sessionStorage.getItem("Name"));
+  var value = sessionStorage.getItem("Name");
+  sessionStorage.removeItem("Name");
+  document.getElementById("output").innerHTML = "";
+  document.getElementById("output").innerHTML =
+    value +
+    "<span class='success'>" +
+    " has been successfully removed from Session" +
+    "</span>";
 }
