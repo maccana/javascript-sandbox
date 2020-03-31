86ec76617fcdc1a42237de20a106947170000b07
Mike <mikeymccann@gmail.com>
2020-03-31 00:36:51 -0400

    Add wav audio playback example

--- local-storage/local-storage.js
+++ local-storage/local-storage.js
@@ -2,16 +2,56 @@
   if (typeof Storage !== "undefined") {
     // Code for localStorage/sessionStorage.
     console.log("Local Storage supported...");
+
+    let dur = 5;
+    let cnt = 0;
+    let notes = [];
+    let timer;
+    notes.push("54");
+    notes.push("32");
+
+    const fileExt = "pic.jpg";
+
+    let JSONreadyNotes = JSON.stringify(notes);
+
     // Sort
     localStorage.setItem("lastname", "Smith");
+    localStorage.setItem("notes", notes);
 
     // Retrieve
     document.getElementById("result").innerHTML = localStorage.getItem(
       "lastname"
     );
+
+    function startTimer() {
+      console.log("timer started ");
+
+      timer = setInterval(doSomething, 2000);
+    }
+
+    // Called by setInterval
+    function doSomething() {
+      cnt += 1;
+      const fileExtension = /\..*/;
+      let newFile = fileExt.replace(fileExtension, "");
+      console.log("F ext ", newFile);
+
+      if (cnt === dur) {
+        console.log("cnt ", cnt);
+        stopTimer();
+      }
+      console.log("Current Loop Count: ", cnt);
+    }
+
+    function stopTimer() {
+      clearInterval(timer);
+      console.log("timer stopped ");
+    }
+
+    startTimer();
   } else {
     alert(
-      "Sorry.Your web browser does not support Local Storage.Please choose another brower to continue."
+      "Sorry,your web browser does not support Local Storage.Please choose another brower to continue."
     );
   }
 })();
