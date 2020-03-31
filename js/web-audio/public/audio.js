84988b77d6b43ef442fe17147aa88e56dd96cfe0
Mike <mikeymccann@gmail.com>
2020-03-31 18:40:44 -0400

    Add playback with Web Audio API

--- js/web-audio/public/audio.js
+++ js/web-audio/public/audio.js
@@ -8,3 +8,25 @@ $(function() {
     $.tmpl("audioTemplate", { src: href }).appendTo(audioElement);
   }
 });
+
+// Playback a file using Web Audio API
+
+const ctx = new AudioContext();
+let audio;
+
+fetch("frogs.wav")
+  .then(data => data.arrayBuffer())
+  .then(arrayBuffer => ctx.decodeAudioData(arrayBuffer))
+  .then(decodedAudio => {
+    audio = decodedAudio;
+  });
+
+function playback() {
+  console.log("Playback called...");
+  const playSound = ctx.createBufferSource();
+  playSound.buffer = audio;
+  playSound.connect(ctx.destination);
+  playSound.start(ctx.currentTime);
+}
+
+window.addEventListener("mousedown", playback);
