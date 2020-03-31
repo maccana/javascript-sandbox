5d0793982430b0784f787f3b4eef2c049d3f649b
Mike <mikeymccann@gmail.com>
2020-03-31 18:35:50 -0400

    Add public folder

---
+++ js/web-audio/public/audio.js
@@ -0,0 +1,10 @@
+// Create and append audio element to DOM with jQuery
+$(function() {
+  var audioElement = $("#audio"),
+    href = audioElement.children("a").attr("href");
+  $.template("audioTemplate", '<audio src="${src}" controls>');
+  if (Modernizr.audio.mp3) {
+    audioElement.empty();
+    $.tmpl("audioTemplate", { src: href }).appendTo(audioElement);
+  }
+});
