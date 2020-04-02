fbac2c55b2615306d1880504062a030ba8fff255
Mike <mikeymccann@gmail.com>
2020-04-02 12:56:13 -0400

    Delete files

--- js/audio.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  var audioElement = $("#audio"),
-    href = audioElement.children("a").attr("href");
-  $.template("audioTemplate", '<audio src="${src}" controls>');
-  if (Modernizr.audio.mp3) {
-    audioElement.empty();
-    $.tmpl("audioTemplate", { src: href }).appendTo(audioElement);
-  }
-});
